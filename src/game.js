90c176795aa04492c4f6af60aee5eee57d7edb00
Eric Ho <eric.h924@gmail.com>
2020-04-22 17:13:00 -0700

    add hearts

--- src/game.js
+++ src/game.js
@@ -11,18 +11,20 @@ class Game {
         this.gameOver = false;
         this.enemies = []
         this.words = new Words();
-
-        this.spawnEnemy = this.spawnEnemy.bind(this)
+        this.health = 3;
+        this.heart = new Image ();
+        this.heart.src = "./images/heart.png";
+        this.startGame = this.startGame.bind(this)
     }
 
 
     startGame() {
-        // this.canvas.removeEventListener("click", game.startGame)
+        this.canvas.removeEventListener("click", this.startGame)
         let input = document.getElementById("user-input");
         input.classList.toggle("hide")
         this.spawnEnemy();
-        this.currentFrame = new Date ()
-        this.animate()
+        this.currentFrame = new Date()
+        this.animate();
     }
 
     spawnEnemy() {
@@ -34,6 +36,7 @@ class Game {
         this.ctx.clearRect(0,0, this.canvas.width, this.canvas.height);
         this.drawEnemies();
         this.checkInput();
+        this.showHealth();
        
     }
 
@@ -52,13 +55,24 @@ class Game {
 
     checkInput() {
         for (let i = 0; i < this.enemies.length; i++) {
-            if (this.input.value === this.enemies[i].word) {
+            if (this.input.value.toUpperCase() === this.enemies[i].word) {
                 this.enemies.splice(this.enemies[i],1)
                 this.input.value = "";
                 this.spawnEnemy();
             }
         }
     }
+
+    showHealth() {
+        let x = 10
+        for (let i = 0; i < this.health; i++ ) {
+            // debugger
+            this.ctx.drawImage(this.heart, x, 5, 50, 50 );
+            x += 55;
+        }
+    }
+
+    
 
 
 }
